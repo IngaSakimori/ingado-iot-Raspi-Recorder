7853d414697e0b566cf1c03b1ce813938b54a6c1
IngaSakimori <47144945+IngaSakimori@users.noreply.github.com>
2022-01-02 13:17:08 +0900

    Update 2_time_set.sh

--- opt/ingado-iot-camera/2_time_set.sh
+++ opt/ingado-iot-camera/2_time_set.sh
@@ -12,11 +12,11 @@
 
 source /opt/ingado-iot-camera/common.conf
 
-date --set @"$(wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | sed -n 4p | cut -d. -f1)"
+date --set @$(timeout -sKILL 10 wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | sed -n 4p | cut -d. -f1)
 
 now_time=`date "+%Y%m%d-%H%M%S"`
 
-echo "$now_time rec started!!" >> $LOG_DIR/rec_script.log
+echo "$now_time time set completed!!" >> $LOG_DIR/rec_script.log
 
 $SCRIPT_DIR/3_mail_start_time.sh &
 exit 0
