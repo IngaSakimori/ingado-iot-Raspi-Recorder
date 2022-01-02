6ca3981a67241e8c35ea231add48936380488b54
IngaSakimori <47144945+IngaSakimori@users.noreply.github.com>
2022-01-02 13:18:19 +0900

    Update 5_rec_start_unknown_time.sh

--- opt/ingado-iot-camera/5_rec_start_unknown_time.sh
+++ opt/ingado-iot-camera/5_rec_start_unknown_time.sh
@@ -12,7 +12,38 @@
 
 source /opt/ingado-iot-camera/common.conf
 
-echo "date is unknown!!" >> $LOG_DIR/rec_script.log
+echo "$now_time date is unknown!!" >> $LOG_DIR/rec_script.log
+
+#NLLモードのフラグをチェックし、有効ならばNLLSETを書き込む
+if [ $NLLFLAG -eq 1 ]
+then
+  now_time=`date "+%Y%m%d-%H%M%S"`
+  echo "$now_time(fake time) $HOSTNAME NLL mode is active. Background convert and USB memory move is stop while recording(raspivid running)" >> $LOG_DIR/rec_script.log
+  NLLSET="_NLL_mode"
+else
+  now_time=`date "+%Y%m%d-%H%M%S"`
+  echo "$now_time(fake time) $HOSTNAME Normal mode is active." >> $LOG_DIR/rec_script.log
+fi
+
+#USBメモリに録画停止ファイルがある場合は、録画を開始せずにエンコードを始める
+if [[ -f /media/pi/RASPI/stop_rec ]]; then
+  # USBメモリのルートにファイル「stop_rec」がある場合は録画を開始せずにエンコードを始める
+  echo "$now_time stop_rec file found. Rec will not start." >> $LOG_DIR/rec_script.log
+  (echo -e "$HOSTNAME fake-time-$now_time stop_rec file found. Rec will not start!! $MAILINFO" | mail -s "$HOSTNAME stop_rec file found. Rec will not start!! fake-time-$now_time" $MAIL_ADD)&
+  rm -f /media/pi/RASPI/stop_rec
+  touch /media/pi/RASPI/stop_rec_success`date "+%Y%m%d_%H%M"`
+  exit 0
+
+fi
+
+#NLLモードのフラグが有功であり、さらにUSBメモリが刺さっている場合、直接USBメモリへ録画ファイルを録画時間無限で書き出す
+if [ $NLLFLAG -eq 1 -a -d /media/pi/RASPI ]
+then
+  echo "$now_time $HOSTNAME NLL mode is active and USB Memory is detected. Rec file record USB memory directly" >> $LOG_DIR/rec_script.log
+  echo "$now_time NLL USB Memory Direct rec started!!" >> $LOG_DIR/rec_script.log
+  (echo -e "$HOSTNAME $now_time NLL USB Memory Direct rec started!! $MAILINFO" | mail -s "$HOSTNAME NLL USB Memory Direct REC STARTED $now_time" $MAIL_ADD)&
+   raspivid -o $USB_MEM_DIR/rec-start-fake-time-$now_time$NLLSET.h264 -fps $FPS -w $WIDE -h $HEIGHT $OPTION $ROTATION $BIT_RATE_FLAG $BIT_RATE -t 99999999999999999 -n $RASPIVID_VERBOSE >> $LOG_DIR/rec_script.log 2>&1
+fi
 
 #指定マイクロ秒ずつのH264を書き出し。電源ぶつ切りにも対応する。
 
@@ -31,22 +62,21 @@ else
 fi
 
   now_time=`date "+%Y%m%d-%H%M%S"`
-  echo "Date is not correct. fake time $now_time rec started!!" >> $LOG_DIR/rec_script.log
-  raspivid -o $SCRIPT_DIR/rec_h264/rec-start-fake-time-$now_time.h264 -fps $FPS -w $WIDE -h $HEIGHT -b $BIT_RATE -t $REC_TIME $PREVIEW
+  echo "$now_time Date is not correct. fake time $now_time rec started!!" >> $LOG_DIR/rec_script.log
+  raspivid -o $SCRIPT_DIR/rec_h264/rec-start-fake-time-$now_time$NLLSET.h264 -fps $FPS -w $WIDE -h $HEIGHT $OPTION $ROTATION $BIT_RATE_FLAG $BIT_RATE -t $REC_TIME $PREVIEW $RASPIVID_VERBOSE  >> $LOG_DIR/rec_script.log 2>&1
 #録画したh264は処理フォルダへ移動する
-  mv $SCRIPT_DIR/rec_h264/rec-start-fake-time-$now_time.h264 $SCRIPT_DIR/rec_convert
+  mv $SCRIPT_DIR/rec_h264/rec-start-fake-time-$now_time$NLLSET.h264 $SCRIPT_DIR/rec_convert
 
-  #dfコマンドで/以下の使用量パーセンテージを書き込む
-/bin/df / | /usr/bin/tail -1 | /bin/sed 's/^.* \([0-9]*\)%.*$/\1/' > $SCRIPT_DIR/disk_use_per.txt
+#dfコマンドで/以下の使用量パーセンテージを書き込む
+#DISK_PERを確認する
+DISK_PER=`/bin/df / | /usr/bin/tail -1 | /bin/sed 's/^.* \([0-9]*\)%.*$/\1/'`
 
-disk_zan=`cat $SCRIPT_DIR/disk_use_per.txt`
-
-  if [ $disk_zan -gt $DISK_STOP_PER ]
+  if [ $DISK_PER -gt $DISK_STOP_PER ]
   then
 
 #容量一杯なら終了する
-now_time=`date "+%Y%m%d-%H%M%S"`
-echo "$now_time raspi DISK_PER_Full. Stopped Rec " >> $LOG_DIR/rec_script.log
+  now_time=`date "+%Y%m%d-%H%M%S"`
+  echo "$HOSTNAME $now_time DISK_used over 95 percent! Stopped Rec " >> $LOG_DIR/rec_script.log
     break
   fi
 
