6aeecd6f85148c6d3a1e654e3d14f605615ab636
IngaSakimori <47144945+IngaSakimori@users.noreply.github.com>
2022-01-02 21:15:34 +0900

    Update 00_initial_copy.sh

--- 00_initial_copy.sh
+++ 00_initial_copy.sh
@@ -30,7 +30,7 @@ apt update
 echo "install some packages"
 echo " "
 
-apt install vim-gtk bc lxshortcut xrdp firefox-esr -y
+apt install vim-gtk sendmail bc lxshortcut xrdp firefox-esr -y
 
 echo "install sysstat. wait few minutes. If you are asked , select [Yes] "
 read -p "Hit enter: "
