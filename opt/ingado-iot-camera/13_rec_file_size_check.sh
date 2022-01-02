8e846da441a9c1e509fc8d86c644d20ee01ddc6e
IngaSakimori <47144945+IngaSakimori@users.noreply.github.com>
2022-01-02 13:15:28 +0900

    Update 13_rec_file_size_check.sh

--- opt/ingado-iot-camera/13_rec_file_size_check.sh
+++ opt/ingado-iot-camera/13_rec_file_size_check.sh
@@ -20,28 +20,29 @@ else
 fi
 
 #録画しているのにファイルサイズが0の場合、ケーブル不良の可能性が高いのでチェックする
-SIZE=`wc -c $SCRIPT_DIR/rec_h264/*.h264 | awk '{print $1}'`
+SIZE=`ls -l $SCRIPT_DIR/rec_h264/*.h264 | awk '{print $5}'`
 if [ ! 0 -eq $SIZE ]; then
   echo "file size OK!"
   echo "exit process"
   exit 0
 else
-  echo "file size 0! check again after 30sec"
+  echo "file size 0! check again after 60sec"
 fi
 
-sleep 30s
+sleep 60s
 
-#一度ダメだった場合、30秒後にもう一度チェックして、それでもダメならメール通知して、録画終了する。
-SIZE2=`wc -c $SCRIPT_DIR/rec_h264/*.h264 | awk '{print $1}'`
+#一度ダメだった場合、60秒後にもう一度チェックして、それでもダメならメール通知して、録画終了する。
+SIZE2=`ls -l $SCRIPT_DIR/rec_h264/*.h264 | awk '{print $5}'`
 if [ ! 0 -eq $SIZE2 ]; then
   echo "file size OK!"
   echo "exit process"
   exit 0
 else
   now_time=`date "+%Y%m%d-%H%M%S"`
-  echo "Rec process is not normal! File size 0! check camera cable and camera unit!" >> $LOG_DIR/rec_error.log
-  echo "Rec process is not normal! File size 0! check camera cable and camera unit!" | mail -s "camera unit or cable fail $now_time" $MAIL_ADD
+  echo "$now_time Rec process is not normal! File size 0! check camera cable and camera unit!" >> $LOG_DIR/rec_script.log
+  (echo -e "$HOSTNAME $now_time Rec process is not normal! File size 0! check camera cable and camera unit! $MAILINFO" | mail -s "$HOSTNAME camera unit or cable fail $now_time" $MAIL_ADD)&
   $SCRIPT_DIR/9_kill_raspi_rec.sh
 #0バイトファイルは録画失敗ディレクトリへ移しておく
+  mkdir $SCRIPT_DIR/rec_convert/failed
   mv $SCRIPT_DIR/rec_h264/*.h264 $SCRIPT_DIR/rec_convert/failed
 fi
